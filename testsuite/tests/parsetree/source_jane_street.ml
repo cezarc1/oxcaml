55063d5d533c80e7bcc74d5d38a4f85d1c7d7a91
Diana Kalinichenko <dkalinichenko@janestreet.com>
2026-06-23 14:45:06 -0400

    Remove redundant modifier annotations

--- testsuite/tests/parsetree/source_jane_street.ml
+++ testsuite/tests/parsetree/source_jane_street.ml
@@ -163,7 +163,7 @@ type t17 : value & value non_pointer
 |}]
 
 type ('a : value mod external_ stateless many unyielding non_float) t18 =
-  ('a : value mod immutable global aliased)
+  ('a : value mod immutable global)
 [%%expect{|
 type ('a : value mod everything non_float) t18 = 'a
 |}]
@@ -1342,15 +1342,15 @@ type existential_abstract =
 |}]
 
 module M : sig
-  kind_ immediate = value mod global many uncontended
-  kind_ immutable_data = value mod uncontended many
-  kind_ immutable = value mod uncontended
+  kind_ immediate = value mod global many
+  kind_ immutable_data = value mod many
+  kind_ immutable = value
   kind_ data = value mod many
   kind_ abstract
 end = struct
-  kind_ immediate = value mod global many uncontended
-  kind_ immutable_data = value mod uncontended many
-  kind_ immutable = value mod uncontended
+  kind_ immediate = value mod global many
+  kind_ immutable_data = value mod many
+  kind_ immutable = value
   kind_ data = value mod many
   kind_ abstract
 end
